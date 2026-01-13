43799bbca95c210b24224288505015a148fa6086
DIMACALI IRENE BIANCA PEREZ 2404851D <2404851D@student.tp.edu.sg>
2026-01-13 15:05:49 +0800

    adding songs album and users in Nav (View)

---
+++ MeowsicLabzz/Components/Domain/BaseDomainModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MeowsicLabzz.Components.Domain
+{
+    public abstract class BaseDomainModel
+    {
+        public int Id { get; set; } 
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string? Createdby { get; set; }
+        public string? UpdatedBy { get; set; }
+    }
+}
