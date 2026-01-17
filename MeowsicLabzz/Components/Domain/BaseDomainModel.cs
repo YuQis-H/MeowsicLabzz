4787278bb54bcf21ae82d964d88c07960bebe8e9
DIMACALI IRENE BIANCA PEREZ 2404851D <2404851D@student.tp.edu.sg>
2026-01-18 01:37:51 +0800

    NotMapped

--- MeowsicLabzz/Components/Domain/BaseDomainModel.cs
+++ MeowsicLabzz/Components/Domain/BaseDomainModel.cs
@@ -5,14 +5,13 @@ namespace MeowsicLabzz.Components.Domain
     public abstract class BaseDomainModel
     {
         public int Id { get; set; }
-
+        [NotMapped]
         public DateTime CreatedAt { get; set; }
-
+        [NotMapped]
         public string? CreatedBy { get; set; }
-
-        
+        [NotMapped]
         public DateTime UpdatedAt { get; set; }
-
+        [NotMapped]
         public string? UpdatedBy { get; set; }
     }
 }
